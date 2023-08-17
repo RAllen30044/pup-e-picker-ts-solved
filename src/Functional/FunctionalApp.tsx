f4a6596762943a47f1d90b9261f5f14ed389a826
Robert <robharmony@hotmail.com>
2023-08-17 15:18:33 -0400

    ready to insert data

--- src/Functional/FunctionalApp.tsx
+++ src/Functional/FunctionalApp.tsx
@@ -1,8 +1,65 @@
+import { useEffect, useState } from "react";
 import { FunctionalCreateDogForm } from "./FunctionalCreateDogForm";
 import { FunctionalDogs } from "./FunctionalDogs";
 import { FunctionalSection } from "./FunctionalSection";
+import { Dog } from "../types";
+import { Requests } from "../api";
 
 export function FunctionalApp() {
+  const [allDogs, setAllDogs] = useState<Dog[]>([]);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const fetchData = () => {
+    setIsLoading(true);
+    return Requests.getAllDogs()
+      .then(setAllDogs)
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  useEffect(() => {
+    setIsLoading(true);
+    fetchData().then(() => {
+      setIsLoading(false);
+    });
+  }, []);
+
+  const createDog = (dog: Omit<Dog, "id">) => {
+    setIsLoading(true);
+    Requests.postDog(dog)
+      .then(() => {
+        fetchData();
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const deleteDog = (id:number) => {
+    setIsLoading(true);
+    Requests.deleteDog(id)
+      .then(() => {
+        fetchData();
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const updateDog = (id:number, favorite:boolean) => {
+    setIsLoading(true);
+    Requests.updateDog(id,favorite)
+      .then(() => {
+        fetchData();
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+
+
   return (
     <div className="App" style={{ backgroundColor: "skyblue" }}>
       <header>
--- src/api.tsx
+++ src/api.tsx
@@ -1,16 +1,36 @@
 export const baseUrl = "http://localhost:3000";
+import { Dog } from "./types";
 
 export const Requests = {
   // should return a promise with all dogs in the database
-  getAllDogs: () => {},
+  getAllDogs: (): Promise<Dog[]> =>
+    fetch(`${baseUrl}/dogs`).then((res) => res.json()),
   // should create a dog in the database from a partial dog object
   // and return a promise with the result
-  postDog: () => {},
+  postDog: (dog: Omit<Dog, "id">) => {
+    return fetch(`${baseUrl}/dogs`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(dog),
+    }).then((res) => res.json());
+  },
 
   // should delete a dog from the database
-  deleteDog: () => {},
+  deleteDog: (id: number) => {
+    return fetch(`${baseUrl}/dogs/${id}`, {
+      method: "DELETE",
+    }).then((res) => res.json());
+  },
 
-  updateDog: () => {},
+  updateDog: (id: number, favorite: boolean) => {
+    return fetch(`${baseUrl}/dogs/${id}`, {
+      method: "PATCH",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        isFavorite: !favorite,
+      }),
+    }).then((res) => res.json());
+  },
 
   // Just a dummy function for use in the playground
   dummyFunction: () => {
